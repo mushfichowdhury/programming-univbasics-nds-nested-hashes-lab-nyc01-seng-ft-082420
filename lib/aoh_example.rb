be242a99effbf3f3e732fb201ad094b919bb58d1
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-20 07:45:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,10 +25,12 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  array[row][]
+  array[row][:key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+  
+  
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
