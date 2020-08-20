8a341f3e75f299ddb327aefcad787b449237b515
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-20 07:45:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  array = [
+  aoh = [
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
@@ -29,7 +29,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  
+  aoh[row][:key] = new_value
   
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
